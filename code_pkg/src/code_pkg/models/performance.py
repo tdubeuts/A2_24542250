ceee6f0c72d8aebf3e1c6911ccd43823f0230192
tdubeuts22 <117575715+tdubeuts22@users.noreply.github.com>
2024-10-05 20:52:38 +1000

    added functions

---
+++ code_pkg/src/code_pkg/models/performance.py
@@ -0,0 +1,129 @@
+# Solution:
+def print_regressor_scores(y_preds, y_actuals, set_name=None):
+    """Print the RMSE and MAE for the provided data
+
+    Parameters
+    ----------
+    y_preds : Numpy Array
+        Predicted target
+    y_actuals : Numpy Array
+        Actual target
+    set_name : str
+        Name of the set to be printed
+
+    Returns
+    -------
+    """
+    from sklearn.metrics import root_mean_squared_error as rmse
+    from sklearn.metrics import mean_absolute_error as mae
+
+    print(f"RMSE {set_name}: {rmse(y_actuals, y_preds)}")
+    print(f"MAE {set_name}: {mae(y_actuals, y_preds)}")
+
+
+
+def print_classifier_scores(y_preds, y_actuals, set_name=None):
+    """Print the Accuracy and F1 score for the provided data.
+    The value of the 'average' parameter for F1 score will be determined according to the number of distinct values of the target variable: 'binary' for bianry classification' or 'weighted' for multi-classs classification
+
+    Parameters
+    ----------
+    y_preds : Numpy Array
+        Predicted target
+    y_actuals : Numpy Array
+        Actual target
+    set_name : str
+        Name of the set to be printed
+    Returns
+    -------
+    """
+    from sklearn.metrics import accuracy_score
+    from sklearn.metrics import f1_score
+    import pandas as pd
+
+    average = 'weighted' if pd.Series(y_actuals).nunique() > 2 else 'binary'
+
+    print(f"Accuracy {set_name}: {accuracy_score(y_actuals, y_preds)}")
+    print(f"F1 {set_name}: {f1_score(y_actuals, y_preds, average=average)}")
+
+def assess_classifier_set(model, features, target, set_name=''):
+    """Save the predictions from a trained model on a given set and print its accuracy and F1 scores
+
+    Parameters
+    ----------
+    model: sklearn.base.BaseEstimator
+        Trained Sklearn model with set hyperparameters
+    features : Numpy Array
+        Features
+    target : Numpy Array
+        Target variable
+    set_name : str
+        Name of the set to be printed
+
+    Returns
+    -------
+    """
+    preds = model.predict(features)
+    print_classifier_scores(y_preds=preds, y_actuals=target, set_name=set_name)
+
+def fit_assess_classifier(model, X_train, y_train, X_val, y_val):
+    """Train a classifier model, print its accuracy and F1 scores on the training and validation set and return the trained model
+
+    Parameters
+    ----------
+    model: sklearn.base.BaseEstimator
+        Instantiated Sklearn model with set hyperparameters
+    X_train : Numpy Array
+        Features for the training set
+    y_train : Numpy Array
+        Target for the training set
+    X_train : Numpy Array
+        Features for the validation set
+    y_train : Numpy Array
+        Target for the validation set
+
+    Returns
+    sklearn.base.BaseEstimator
+        Trained model
+    -------
+    """
+    model.fit(X_train, y_train)
+    assess_classifier_set(model, X_train, y_train, set_name='Training')
+    assess_classifier_set(model, X_val, y_val, set_name='Validation')
+    return model
+
+
+def split_sets_random(features, target, test_ratio=0.2):
+    """Split sets randomly
+
+    Parameters
+    ----------
+    features : pd.DataFrame
+        Input dataframe
+    target : pd.Series
+        Target column
+    test_ratio : float
+        Ratio used for the validation and testing sets (default: 0.2)
+
+    Returns
+    -------
+    Numpy Array
+        Features for the training set
+    Numpy Array
+        Target for the training set
+    Numpy Array
+        Features for the validation set
+    Numpy Array
+        Target for the validation set
+    Numpy Array
+        Features for the testing set
+    Numpy Array
+        Target for the testing set
+    """
+    from sklearn.model_selection import train_test_split
+
+    val_ratio = test_ratio / (1 - test_ratio)
+    X_data, X_test, y_data, y_test = train_test_split(features, target, test_size=test_ratio, random_state=8)
+    X_train, X_val, y_train, y_val = train_test_split(X_data, y_data, test_size=val_ratio, random_state=8)
+
+    return X_train, y_train, X_val, y_val, X_test, y_test
